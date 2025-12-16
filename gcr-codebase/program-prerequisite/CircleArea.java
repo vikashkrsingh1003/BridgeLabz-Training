1252bec5c68c0a09986606f4daf77a1f78b43ae5
vikash <vksingh1062003@gmail.com>
2025-12-16 17:02:09 +0530

    [vikash] : added find area of circle program

---
+++ gcr-codebase/program-prerequisite/CircleArea.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CircleArea {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        double radius = sc.nextDouble();
+
+        double area = 3.14 * Math.pow(radius, 2);
+
+        System.out.println("The area of the circle is: " + area);
+        sc.close();
+    }
+}
