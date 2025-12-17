6ef7e3e05780f7c5378b97b922a61d8e7be953ad
vikash <vksingh1062003@gmail.com>
2025-12-17 17:06:04 +0530

    [vikash] : added code of finding simple intrest

--- gcr-codebase/program-prerequisite/SimpleInterest.java
+++ gcr-codebase/program-prerequisite/SimpleInterest.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 class SimpleInterest{
     public static void main(String[] args) {
-
+   
+    //Take the input principle , rate and time from user:
         Scanner sc = new Scanner(System.in);
-
         double principal = sc.nextDouble();
         double rate = sc.nextDouble();
         double time = sc.nextDouble();
-
+     
+     //Calclate the simple intrest using formula
         double simpleInterest = (principal * rate * time) / 100;
-
+     
+     //Displaying the final intrest money:
         System.out.println("simple intrest is : " + simpleInterest);
        
     }
