98d1ce9815dd2c183d4cfa93f3795cfadcb254fb
vikash <vksingh1062003@gmail.com>
2025-12-17 12:20:17 +0530

    [vikash] : program to find the harry age

---
+++ gcr-codebase/programming-elements/level-1/FindingAge.java
@@ -0,0 +1,13 @@
+
+class FindingAge{
+    public static void main(String args[]){
+
+        int harryBrithYear = 2000;
+        int currentYear = 2024;
+
+        int ageOfHarry = currentYear - harryBrithYear ;
+
+        System.out.println("Harry's age in 2024 is : "+ ageOfHarry);
+
+    }
+}
