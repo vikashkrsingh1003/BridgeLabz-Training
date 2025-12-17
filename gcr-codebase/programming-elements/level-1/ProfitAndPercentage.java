832878ea57b3707b0b53a3c5de2f3bdae96c17ef
vikash <vksingh1062003@gmail.com>
2025-12-17 17:00:38 +0530

    [vikash] : added the code for profit and profitPercentage

--- gcr-codebase/programming-elements/level-1/ProfitAndPercentage.java
+++ gcr-codebase/programming-elements/level-1/ProfitAndPercentage.java
@@ -4,17 +4,14 @@ public class ProfitAndPercentage{
       //intialize the costprice and sellingprice;
         int sellingPrice = 191;
         int costPrice = 129;
-      //find the profit
+
+      //find the profit and profit Percentage
         int profit = sellingPrice - costPrice ;
-      //find the profit Percentage 
         double profitPercentage = ((double)profit/costPrice) * 100;
        
-       // final print the profit and profitPercentage;
+      // Displaying  the profit and profitPercentage in one line ;
         System.out.println("The Cost Price is INR " + costPrice +"and Selling Price is INR "+ sellingPrice);
-
         System.out.println("The Profit is INR " + profit + " and the Profit Percentage is " + profitPercentage);
 
-    }
-
-
-    }
+         }
+     }
