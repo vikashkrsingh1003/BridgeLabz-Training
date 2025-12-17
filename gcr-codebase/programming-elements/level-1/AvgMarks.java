d9cccb5eb50d385ce50c0dd70b4b69c67554e075
vikash <vksingh1062003@gmail.com>
2025-12-17 12:22:48 +0530

    [vikash] : program for find avgMarks

---
+++ gcr-codebase/programming-elements/level-1/AvgMarks.java
@@ -0,0 +1,15 @@
+
+class AvgMarks{
+    public static void main(String args[]){
+
+        int Math =94;
+        int physics = 95;
+        int chemistry = 96;
+
+        int avgMarks = (Math+physics+ chemistry)/3;
+
+        System.out.println("Sam’s average mark in PCM is " + avgMarks);
+
+
+    }
+}
