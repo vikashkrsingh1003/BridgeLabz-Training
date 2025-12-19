45f0d3119b54fa643f1e9cd1d890ccbec2d7c02f
vikash <vksingh1062003@gmail.com>
2025-12-19 11:23:37 +0530

    [vikash] : added the program for calculate sum until zero

---
+++ gcr-codebase/control-flows/Level-01/SumUntilZero.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumUntilZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double total = 0.0;   
+        double number = sc.nextDouble();
+        
+        // While user does not enter 0
+        while (number !=  0) {
+            total += number;  
+            
+            //for nextInput 
+            number = sc.nextDouble();
+        }
+
+        //Display the total
+        System.out.println("Total sum = " + total);
+
+       
+    }
+}
