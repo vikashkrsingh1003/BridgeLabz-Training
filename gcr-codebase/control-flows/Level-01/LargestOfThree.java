6f293eebdb9a2b5c8f9a279fea35247ff975b2b9
vikash <vksingh1062003@gmail.com>
2025-12-19 11:20:38 +0530

    [vikash] : added the program for find largest of three  number

---
+++ gcr-codebase/control-flows/Level-01/LargestOfThree.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class LargestOfThree {
+    public static void main(String args[]) {
+        
+        // Take input from user
+        Scanner sc = new Scanner(System.in);
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+        int number3 = sc.nextInt();
+        
+        // Check and display results
+        if (number1 > number2 && number1 > number3) {
+            System.out.println("Is the first number the largest? Yes");
+        } else {
+            System.out.println("Is the first number the largest? No");
+        }
+        
+        if (number2 > number1 && number2 > number3) {
+            System.out.println("Is the second number the largest? Yes");
+        } else {
+            System.out.println("Is the second number the largest? No");
+        }
+        
+        if (number3 > number1 && number3 > number2) {
+            System.out.println("Is the third number the largest? Yes");
+        } else {
+            System.out.println("Is the third number the largest? No");
+        }
+        
+    }
+}
