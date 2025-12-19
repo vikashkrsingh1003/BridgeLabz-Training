3affcc1f87912175506100a2c5bac43f45cc6ac7
vikash <vksingh1062003@gmail.com>
2025-12-19 11:09:18 +0530

    [vikash] : added the program for count down2

---
+++ gcr-codebase/control-flows/Level-01/CountDown2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class CountDown2 {
+    public static void main(String args[]){
+
+        //Take input counter from user
+        Scanner sc = new Scanner(System.in);
+        int counter = sc.nextInt();
+
+        //Countdown using while loop
+          while (counter >= 1) {
+            System.out.println("Countdown: "+ counter);
+            counter--; 
+        }
+
+        sc.close();
+
+
+    }
+}
