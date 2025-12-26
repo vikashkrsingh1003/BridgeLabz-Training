6db0fb1caa9a24afa381af2f8291ee36fa6b421d
vikash <vksingh1062003@gmail.com>
2025-12-26 15:47:59 +0530

    [vikash] : added the program for Two sum in leetcode pr-01

---
+++ leet-code-codebase/Easy-Level/Array-Basics/TwoSum.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class TwoSum {
+    
+    //methode to find two sum
+    public static int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+   
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target value: ");
+        int target = sc.nextInt();
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println("Output indices:");
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
