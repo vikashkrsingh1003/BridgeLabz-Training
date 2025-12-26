9deb7cf3a2122a6e3323f774ba9532560a4416f6
vikash <vksingh1062003@gmail.com>
2025-12-26 12:28:58 +0530

    [vikash] : added the program for check palindrome number

---
+++ gcr-codebase/Extras/String-Problem-Statement/PalindromeChecker.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    static boolean isPalindrome(String str) {
+        int start = 0, end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("Palindrome String");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+
+        sc.close();
+    }
+}
