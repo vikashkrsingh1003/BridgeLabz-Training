728521fb4e27e3540858d5bf2207d8ab2f9ac0db
vikash <vksingh1062003@gmail.com>
2025-12-26 12:26:33 +0530

    [vikash] : added the program for find gcd and lcm calculator

---
+++ gcr-codebase/Extras/String-Problem-Statement/GCDLCMCalculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class GCDLCMCalculator {
+
+    static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int x = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int y = sc.nextInt();
+
+        System.out.println("GCD = " + gcd(x, y));
+        System.out.println("LCM = " + lcm(x, y));
+
+        sc.close();
+    }
+}
