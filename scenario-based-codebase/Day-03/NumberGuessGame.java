541370a85d7f28db70a441bf4ff3dd636aa831dc
vikash <vksingh1062003@gmail.com>
2025-12-30 00:30:20 +0530

    [vikash]:added the program for Number guessing game

---
+++ scenario-based-codebase/Day-03/NumberGuessGame.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class NumberGuessGame{
+
+    public static void main(String[] args) {
+        
+        int min = 1;
+        int max = 100;
+        Random random = new Random();
+        int number = random.nextInt(max - min + 1) + min;
+        int wrongAttempts=0;
+
+        do{
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your Guess number : ");
+            int guessNum = sc.nextInt();
+
+            if(guessNum == number){
+                System.out.println("You guess right number ");
+                break;
+            }
+            if(guessNum < number){
+                System.out.println("Guess Two Low");
+                wrongAttempts++;
+            }else{
+              System.out.println("Guess Two High");
+            }
+
+        }while(wrongAttempts < 5);
+
+        if(wrongAttempts>5){
+            System.out.println("Wrong Attempts limit exceeded");
+        }
+
+
+    }
+}
