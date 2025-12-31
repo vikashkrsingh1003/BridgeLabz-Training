16561a74aa7cb90632908529106b89769ec3542d
vikash <vksingh1062003@gmail.com>
2025-12-31 13:27:28 +0530

    [vikash] : added the program for convert ToLower case

---
+++ leet-code-codebase/Easy-Level/Strings-Basics/ToLowerCase.java
@@ -0,0 +1,9 @@
+public class ToLowerCase {
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("Hello WORLD"));
+    }
+}
