b4809b4f73f695bd0e34d1b8b100c90aadba4a8b
vikash <vksingh1062003@gmail.com>
2025-12-31 12:49:29 +0530

    [vikash]: added the program for recharge Simulator

---
+++ scenario-based-codebase/Day-03/RechargeSimulator.java
@@ -0,0 +1,13 @@
+/*Phone Recharge Simulator 
+Take the user's mobile operator and amount.
+● Use a switch to display offers.
+● Loop to allow repeated recharges.
+● Show balance after each recharge. */
+
+import java.util.*;
+public class RechargeSimulator {
+     
+    public static void main(String[] args) {
+         
+    }
+}
