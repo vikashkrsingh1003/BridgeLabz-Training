5f30da93ce9865b05179f54f686ce374c5a3118b
vikash <vksingh1062003@gmail.com>
2026-01-21 11:43:15 +0530

    [vikash] : added the program for find union intersection

---
+++ java-collection-practice/gcr-codebase/JavaCollections/src/com/collections/setinterface/UnionIntersection.java
@@ -0,0 +1,36 @@
+package com.collections.setinterface;
+import java.util.*;
+
+public class UnionIntersection {
+ public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+
+     System.out.print("Enter number of elements in Set 1: ");
+     int n1 = sc.nextInt();
+     Set<Integer> set1 = new HashSet<>();
+     System.out.println("Enter elements of Set 1:");
+     for (int i = 0; i < n1; i++) {
+         set1.add(sc.nextInt());
+     }
+
+     System.out.print("Enter number of elements in Set 2: ");
+     int n2 = sc.nextInt();
+     Set<Integer> set2 = new HashSet<>();
+     System.out.println("Enter elements of Set 2:");
+     for (int i = 0; i < n2; i++) {
+         set2.add(sc.nextInt());
+     }
+
+     Set<Integer> union = new HashSet<>(set1);
+     union.addAll(set2);
+
+     Set<Integer> intersection = new HashSet<>(set1);
+     intersection.retainAll(set2);
+
+     System.out.println("Union: " + union);
+     System.out.println("Intersection: " + intersection);
+     sc.close();
+ }
+}
+
+
