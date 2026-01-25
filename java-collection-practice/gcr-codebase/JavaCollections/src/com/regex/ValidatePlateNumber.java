98bee03fe181b6f27c1ddedd1d466e8b8f82b225
vikash <vksingh1062003@gmail.com>
2026-01-26 00:12:51 +0530

    [vikash]: added the program for validate  License Plate Number

---
+++ java-collection-practice/gcr-codebase/JavaCollections/src/com/regex/ValidatePlateNumber.java
@@ -0,0 +1,23 @@
+package com.regex;
+
+import java.util.regex.*;
+
+public class ValidatePlateNumber {
+
+	public static void main(String[] args) {
+
+		String regex = "^[A-Z]{2}[0-9]{4}$";
+		
+		Pattern pattern = Pattern.compile(regex);
+		
+		String input = "AB0123";
+		Matcher matcher = pattern.matcher(input);
+		
+		if(matcher.matches())
+			System.out.println("Valid Number");
+		else
+			System.out.println("Invalid Number");
+		
+	}
+
+}
