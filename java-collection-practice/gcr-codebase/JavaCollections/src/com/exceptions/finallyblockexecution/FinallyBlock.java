a26dca6f4981d47d0cdb11b2c79a87538c5c4eec
vikash <vksingh1062003@gmail.com>
2026-01-25 22:26:13 +0530

    [vikash]: added the program for exceptions (finally block execution)

---
+++ java-collection-practice/gcr-codebase/JavaCollections/src/com/exceptions/finallyblockexecution/FinallyBlock.java
@@ -0,0 +1,29 @@
+package com.exceptions.finallyblockexecution;
+
+import java.util.Scanner;
+
+public class FinallyBlock {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter first number: ");
+            int a = sc.nextInt();
+
+            System.out.print("Enter second number: ");
+            int b = sc.nextInt();
+
+            int result = a / b;
+            System.out.println("Result: " + result);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Cannot divide by zero");
+
+        } finally {
+            System.out.println("Operation completed");
+            sc.close();
+        }
+    }
+}
