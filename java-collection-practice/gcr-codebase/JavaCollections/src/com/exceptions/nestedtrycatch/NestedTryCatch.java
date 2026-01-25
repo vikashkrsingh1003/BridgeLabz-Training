4f6d5565e3c3c77fd2c20b5fdff30e14398058cf
vikash <vksingh1062003@gmail.com>
2026-01-25 22:27:22 +0530

    [vikash]: added the program for exceptions (nested try catch)

---
+++ java-collection-practice/gcr-codebase/JavaCollections/src/com/exceptions/nestedtrycatch/NestedTryCatch.java
@@ -0,0 +1,35 @@
+package com.exceptions.nestedtrycatch;
+
+import java.util.Scanner;
+
+public class NestedTryCatch {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        try {
+            System.out.print("Enter array index: ");
+            int index = sc.nextInt();
+
+            try {
+                System.out.print("Enter divisor: ");
+                int divisor = sc.nextInt();
+
+                int result = arr[index] / divisor;
+                System.out.println("Result: " + result);
+
+            } catch (ArithmeticException e) {
+                System.out.println("Cannot divide by zero!");
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid array index!");
+        }
+
+        sc.close();
+    }
+}
+
