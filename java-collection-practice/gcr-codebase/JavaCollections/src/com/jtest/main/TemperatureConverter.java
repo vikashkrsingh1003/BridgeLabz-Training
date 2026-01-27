1e38573e137a84327099538bf4d3df43251f8bff
vikash <vksingh1062003@gmail.com>
2026-01-27 23:53:32 +0530

    [vikash]:Added temperatureconverter program

---
+++ java-collection-practice/gcr-codebase/JavaCollections/src/com/jtest/main/TemperatureConverter.java
@@ -0,0 +1,13 @@
+package com.jtest.main;
+
+public class TemperatureConverter {
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+}
+
