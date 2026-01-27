5456178a2d6ab905f4321e57d8f2aae7c45c837d
vikash <vksingh1062003@gmail.com>
2026-01-27 23:44:19 +0530

    [vikash]: added the program for finding repeating word

--- java-collection-practice/gcr-codebase/JavaCollections/src/com/regex/FindRepeatingWords.java
+++ java-collection-practice/gcr-codebase/JavaCollections/src/com/regex/FindRepeatingWords.java
@@ -5,18 +5,18 @@ import java.util.regex.Pattern;
 
 public class FindRepeatingWords {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    	String regex = "\\b(\\w+)\\s+\\1\\b";
+		String regex = "\\b(\\w+)\\s+\\1\\b";
 
-        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 
-        String input = "This is  is a repeated repeated word test.";
+		String input = "This is  is a repeated repeated word test.";
 
-        Matcher matcher = pattern.matcher(input);
+		Matcher matcher = pattern.matcher(input);
 
-        while (matcher.find()) {
-            System.out.println(matcher.group(1)); // print the repeated word
-        }
-    }
+		while (matcher.find()) {
+			System.out.println(matcher.group(1)); // print the repeated word
+		}
+	}
 }
