ae24e042e79d8e5291a166147dcf83df199806a9
vikash <vksingh1062003@gmail.com>
2026-01-27 23:44:50 +0530

    [vikash]: added the program for velidate number plate

--- java-collection-practice/gcr-codebase/JavaCollections/src/com/regex/ValidatePlateNumber.java
+++ java-collection-practice/gcr-codebase/JavaCollections/src/com/regex/ValidatePlateNumber.java
@@ -7,17 +7,17 @@ public class ValidatePlateNumber {
 	public static void main(String[] args) {
 
 		String regex = "^[A-Z]{2}[0-9]{4}$";
-		
+
 		Pattern pattern = Pattern.compile(regex);
-		
+
 		String input = "AB0123";
 		Matcher matcher = pattern.matcher(input);
-		
-		if(matcher.matches())
+
+		if (matcher.matches())
 			System.out.println("Valid Number");
 		else
 			System.out.println("Invalid Number");
-		
+
 	}
 
 }
