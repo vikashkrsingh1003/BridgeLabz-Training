95c04beeac422ea179ed312514173b1e76a72305
vikash <vksingh1062003@gmail.com>
2026-08-02 20:28:23 +0530

    Add refresher training files

---
+++ Core-Java-Pactice/Week-01/Day-02/AddTwoNumber.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class AddTwoNumber {
+    public static void main(String[] args) {
+
+       Scanner sc = new Scanner(System.in);
+       int A = sc.nextInt();
+       int B = sc.nextInt();
+
+       int sum = A+B;
+
+        System.out.println("Sum of Two Number : "  + sum);
+    }
+}
